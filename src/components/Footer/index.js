6d16a4707dd953e676232d6cae839cd9c39f55a0
Shay Wagner <srwagner916@gmail.com>
2021-07-16 17:48:39 -0500

    added icon links to footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function footer() {
+  return (
+    <footer className="flex justify-center bg-gray-400">
+      <div className="text-5xl">
+        <a href="https://github.com/srwagner916" target="_blank" title="github profile" rel="noreferrer" className="mx-4">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/shay-wagner-067416206/" target="_blank" title="linkedIn profile" rel="noreferrer" className="mx-4">
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default footer;
