7001fc968c4d652c4b4611201af1a4112fb7cff7
Shay Wagner <srwagner916@gmail.com>
2021-07-16 18:43:24 -0500

    about me section is in place

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
+      <AboutMe />
       <Footer />
     </div>
   );
---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function AboutMe() {
+  return(
+    <main className="flex-grow my-6 mx-10 px-10">
+      <h2 className="text-4xl">About Me</h2>
+      <div className="my-4">
+        <p className="text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis ornare ligula, tincidunt
+        porttitor massa. Curabitur sed metus at felis tempor tincidunt vitae quis arcu. Donec ornare
+        dui et diam accumsan commodo. Quisque mi ante, luctus quis mauris ac, condimentum porttitor
+        ipsum. Ut ultricies dignissim orci id viverra. Ut vitae neque ac nunc lacinia porta. Proin nec
+        metus enim. Donec efficitur ex sit amet massa pulvinar ullamcorper. Proin eleifend tortor sed dui
+        dictum, sed viverra tortor eleifend. In ullamcorper nulla eget nisl ullamcorper, id auctor justo
+        porttitor. Donec pulvinar mollis ullamcorper. Etiam metus sapien, posuere sit amet posuere a, auctor
+        quis lectus. Vestibulum nec nisl metus. Etiam posuere ipsum est, nec vestibulum diam consectetur quis.
+        Etiam in arcu convallis sapien venenatis ultrices. Curabitur euismod, justo eget auctor lobortis,
+        libero metus tempus lorem, rutrum iaculis lacus orci eu tellus.
+
+        Vestibulum porttitor iaculis ex et finibus. Nulla nec ipsum quis sapien efficitur consequat. Phasellus
+        id fringilla nisi. Duis malesuada lorem id quam lacinia, eget viverra ante eleifend. Nam bibendum
+        porttitor lectus, ut sodales metus placerat in. Sed ante arcu, vulputate quis hendrerit id, convallis
+        nec nisl. Cras nec euismod tortor. Quisque non augue ullamcorper, tincidunt velit quis, tincidunt arcu.
+        Suspendisse condimentum nisi at imperdiet tempus. Nulla vel finibus augue. Nam porttitor tellus risus,
+        ullamcorper tempor eros mollis nec. Vivamus sit amet nulla ultrices, fermentum nisi a, aliquam neque.
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default AboutMe;
