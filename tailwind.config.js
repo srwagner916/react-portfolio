a4fb68612b6c94f7f4f7328ee454bdc070427d9c
Shay Wagner <srwagner916@gmail.com>
2021-07-18 22:11:46 -0500

    trying another path

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,13 +16,13 @@ module.exports = {
     },
     extend: {
       backgroundImage: theme => ({
-        'hero-image': "url('./images/hero-image.jpg')",
-        'celiac-tackboard-image': "url('./images/celiac-tackboard.png')",
-        'on-hand-cooking-image': "url('./images/on-hand-cooking.png')",
-        'weather-dashboard-image': "url('./images/weather-dashboard.png')",
-        'tech-blog-image': "url('./images/tech-blog-image.png')",
-        'readme-generator-image': "url('./images/README-generator.png')",
-        'code-quiz-image': "url('./images/code-quiz.png')"
+        'hero-image': "url('/images/hero-image.jpg')",
+        'celiac-tackboard-image': "url('/images/celiac-tackboard.png')",
+        'on-hand-cooking-image': "url('/images/on-hand-cooking.png')",
+        'weather-dashboard-image': "url('/images/weather-dashboard.png')",
+        'tech-blog-image': "url('/images/tech-blog-image.png')",
+        'readme-generator-image': "url('/images/README-generator.png')",
+        'code-quiz-image': "url('/images/code-quiz.png')"
       })
     },
   },
