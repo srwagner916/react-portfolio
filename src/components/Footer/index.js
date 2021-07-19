87654e3e099ae0f668cd72d2253c3e8fa29544bf
Shay Wagner <srwagner916@gmail.com>
2021-07-18 21:23:28 -0500

    added stack overflow icon

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,6 +10,9 @@ function footer() {
         <a href="https://www.linkedin.com/in/shay-wagner-067416206/" target="_blank" title="linkedIn profile" rel="noreferrer" className="mx-4">
           <i className="fab fa-linkedin"></i>
         </a>
+        <a href="https://stackoverflow.com/users/15186060/srwagner916" target="_blank" title="Stack Overflow profile" rel="noreferrer" className="mx-4">
+        <i class="fab fa-stack-overflow"></i>
+        </a>
       </div>
     </footer>
   )
